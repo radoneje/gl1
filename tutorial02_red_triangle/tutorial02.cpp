29f437709a3c74b61e1d559ef99b124ceebbda06
radoneje <den.shevchenko@gmail.com>
2022-04-16 18:24:20 +0300

    ddddf

--- tutorial02_red_triangle/tutorial02.cpp
+++ tutorial02_red_triangle/tutorial02.cpp
@@ -45,6 +45,72 @@ static void logging(const char *fmt, ...)
     va_end( args );
     fprintf( stderr, "\n" );
 }
+static int decode_packet(AVPacket *pPacket, AVCodecContext *pCodecContext, AVFrame *pFrame)
+{
+    // Supply raw packet data as input to a decoder
+    // https://ffmpeg.org/doxygen/trunk/group__lavc__decoding.html#ga58bc4bf1e0ac59e27362597e467efff3
+    int response = avcodec_send_packet(pCodecContext, pPacket);
+
+    if (response < 0) {
+        logging("Error while sending a packet to the decoder: %s", av_err2str(response));
+        return response;
+    }
+
+    while (response >= 0)
+    {
+        // Return decoded output data (into a frame) from a decoder
+        // https://ffmpeg.org/doxygen/trunk/group__lavc__decoding.html#ga11e6542c4e66d3028668788a1a74217c
+        response = avcodec_receive_frame(pCodecContext, pFrame);
+        if (response == AVERROR(EAGAIN) || response == AVERROR_EOF) {
+            break;
+        } else if (response < 0) {
+            logging("Error while receiving a frame from the decoder: %s", av_err2str(response));
+            return response;
+        }
+
+        if (response >= 0) {
+            logging(
+                    "Frame %d (type=%c, size=%d bytes, format=%d) pts %d key_frame %d [DTS %d]",
+                    pCodecContext->frame_number,
+                    av_get_picture_type_char(pFrame->pict_type),
+                    pFrame->pkt_size,
+                    pFrame->format,
+                    pFrame->pts,
+                    pFrame->key_frame,
+                    pFrame->coded_picture_number
+            );
+
+            char frame_filename[1024];
+            snprintf(frame_filename, sizeof(frame_filename), "%s-%d.pgm", "frame", pCodecContext->frame_number);
+            // Check if the frame is a planar YUV 4:2:0, 12bpp
+            // That is the format of the provided .mp4 file
+            // RGB formats will definitely not give a gray image
+            // Other YUV image may do so, but untested, so give a warning
+            if (pFrame->format != AV_PIX_FMT_YUV420P)
+            {
+                logging("Warning: the generated file may not be a grayscale image, but could e.g. be just the R component if the video format is RGB");
+            }
+            // save a grayscale frame into a .pgm file
+            save_gray_frame(pFrame->data[0], pFrame->linesize[0], pFrame->width, pFrame->height, frame_filename);
+        }
+    }
+    return 0;
+}
+
+static void save_gray_frame(unsigned char *buf, int wrap, int xsize, int ysize, char *filename)
+{
+    FILE *f;
+    int i;
+    f = fopen(filename,"w");
+    // writing the minimal required header for a pgm file format
+    // portable graymap format -> https://en.wikipedia.org/wiki/Netpbm_format#PGM_example
+    fprintf(f, "P5\n%d %d\n%d\n", xsize, ysize, 255);
+
+    // writing line by line
+    for (i = 0; i < ysize; i++)
+        fwrite(buf + i * wrap, 1, xsize, f);
+    fclose(f);
+}
 
 int main( void )
 {
@@ -162,13 +228,13 @@ int main( void )
 
     int response = 0;
     int how_many_packets_to_process = 8;
-    logging("how_many_packets_to_process->pts" );
+
     while (av_read_frame(pFormatContext, pPacket) >= 0)
     {
         // if it's the video stream
         if (pPacket->stream_index == video_stream_index) {
             logging("AVPacket->pts %" PRId64, pPacket->pts);
-            response = 0;//decode_packet(pPacket, pCodecContext, pFrame);
+            response = decode_packet(pPacket, pCodecContext, pFrame);
             if (response < 0)
                 break;
             // stop it, otherwise we'll be saving hundreds of frames
@@ -184,7 +250,7 @@ int main( void )
     av_packet_free(&pPacket);
     av_frame_free(&pFrame);
     avcodec_free_context(&pCodecContext);
-    return 0;
+
 
 
     // Initialise GLFW
