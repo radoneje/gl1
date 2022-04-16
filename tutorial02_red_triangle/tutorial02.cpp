53d4bd581fc47f5052c95021503527a752248657
radoneje <den.shevchenko@gmail.com>
2022-04-16 12:23:47 +0300

    dddd

--- tutorial02_red_triangle/tutorial02.cpp
+++ tutorial02_red_triangle/tutorial02.cpp
@@ -17,8 +17,47 @@ GLFWwindow* window;
 using namespace glm;
 
 #include <common/shader.hpp>
+void Reshape(int width, int height)
+{
+    glViewport(0, 0, width, height);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(-1, 1, -1, 1);
+    glMatrixMode(GL_MODELVIEW);
+}
 
-int main( void )
+void Draw(void)
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glColor3f(0.0f, 0.0f, 1.0f);
+    glLineWidth(1);
+
+    glBegin(GL_LINES);
+    glVertex2f(0, 0.5f);
+    glVertex2f(0, -0.5f);
+    glEnd();
+
+    glFlush();
+}
+
+int main(int argc, char *argv[])
+{
+    glutInit(&argc, argv);
+    glutInitWindowSize(400, 300);
+    glutInitWindowPosition(100, 100);
+
+    glutInitDisplayMode(GLUT_RGB);
+    glutCreateWindow("Romka Demo");
+
+    glutReshapeFunc(Reshape);
+    glutDisplayFunc(Draw);
+    glClearColor(0, 0, 0, 0);
+
+    glutMainLoop();
+    return 0;
+}
+int v_main( void )
 {
 	// Initialise GLFW
 	if( !glfwInit() )
