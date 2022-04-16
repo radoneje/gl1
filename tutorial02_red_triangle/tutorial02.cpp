5f5f180c63e716fbbd98848f033a647b9536750c
radoneje <den.shevchenko@gmail.com>
2022-04-16 16:23:02 +0300

    ddddf

--- tutorial02_red_triangle/tutorial02.cpp
+++ tutorial02_red_triangle/tutorial02.cpp
@@ -94,6 +94,8 @@ int main( void )
             1.0f,  1.0f,  0.0f,
             1.0f,  1.0f,  0.0f,
             1.0f,  1.0f,  0.0f,
+            0.0f,  1.0f,  0.0f,
+            0.0f,  1.0f,  0.0f,
 
     };
 
@@ -139,6 +141,7 @@ int main( void )
 
 
         glDrawArrays(GL_TRIANGLES, 0, 3*2);
+        glDrawArrays(GL_LINES, 6, 3);
         //glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
 
         glDisableVertexAttribArray(0);
