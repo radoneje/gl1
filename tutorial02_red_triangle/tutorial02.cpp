7ae5bf4943caad6922da6781ce255454d9e508cb
radoneje <den.shevchenko@gmail.com>
2022-04-16 11:40:13 +0300

    dddd

--- tutorial02_red_triangle/tutorial02.cpp
+++ tutorial02_red_triangle/tutorial02.cpp
@@ -82,7 +82,7 @@ float i=-1.0f;
                 1.0f, -1.0f, 0.0f,
                 0.0f,  i, 0.0f,
         };
-        stdout<< i << "\n";
+        stdout<< std::to_string(i) << "\n";
         glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
 
 		// Clear the screen
