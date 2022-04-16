669df39a95049f8225e5d24d1fbf105939af1252
radoneje <den.shevchenko@gmail.com>
2022-04-16 11:34:52 +0300

    dddd

--- tutorial02_red_triangle/tutorial02.cpp
+++ tutorial02_red_triangle/tutorial02.cpp
@@ -74,8 +74,14 @@ int main( void )
 	glGenBuffers(1, &vertexbuffer);
 	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
 
-
+int i=0;
 	do{
+        i=i++;
+        static const GLfloat g_vertex_buffer_data[] = {
+                -1.0f, -1.0f, 0.0f,
+                1.0f, -1.0f, 0.0f,
+                0.0f,  i*0.01f, 0.0f,
+        };
 
         glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
 
