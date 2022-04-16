aa81f30e890f382d540e656fd66e40e387ee2962
radoneje <den.shevchenko@gmail.com>
2022-04-16 17:42:30 +0300

    ddddf

--- tutorial02_red_triangle/tutorial02.cpp
+++ tutorial02_red_triangle/tutorial02.cpp
@@ -36,7 +36,7 @@ int main( void )
         return -1;
     }
     printf(" allocate memory for Format Context\n");
-    if (avformat_open_input(&pFormatContext, '/tmp/vcbr.mp4
+    if (avformat_open_input(&pFormatContext, '/tmp/vcbr.mp4', NULL, NULL) != 0) {
         printf("ERROR could not open the file");
         return -1;
     }
