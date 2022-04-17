c1725442f86b389f3636bbf849f0bbdd242b4f7f
radoneje <den.shevchenko@gmail.com>
2022-04-17 23:01:24 +0300

    ddddf

--- tutorial02_red_triangle/tutorial02.cpp
+++ tutorial02_red_triangle/tutorial02.cpp
@@ -302,11 +302,11 @@ int work(){
                 }
 
             }
-            av_frame_free(pRGBFrame);
-            av_packet_unref(pkt);
+            av_frame_free(&pRGBFrame);
+            av_packet_unref(&pkt);
         }
-    avcodec_free_context(ctx_codec);
-    avformat_close_input(ctx_format);
+    avcodec_free_context(&ctx_codec);
+    avformat_close_input(&ctx_format);
 
 
 
