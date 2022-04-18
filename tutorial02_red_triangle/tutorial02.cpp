6affc08e971181aae1313c86263d222680b6eb78
radoneje <den.shevchenko@gmail.com>
2022-04-18 23:03:04 +0300

    ddddf

--- tutorial02_red_triangle/tutorial02.cpp
+++ tutorial02_red_triangle/tutorial02.cpp
@@ -228,6 +228,7 @@ int work() {
                 finalFrameData.data = pRGBFrame->data[0];
                 finalFrameData.frameNumber = ctx_codec->frame_number;
                 finalFrameData_lock.unlock();
+                std::cout << "sizeof"<< sizeof(pRGBFrame->data[0]) <<std::endl;
                 av_frame_unref(frame);
 
             }
