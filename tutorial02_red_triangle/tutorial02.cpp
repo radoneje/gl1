405fab5aa2a56e9a6ff6eae00f160b68f56df547
radoneje <den.shevchenko@gmail.com>
2022-04-18 14:56:05 +0300

    ddddf

--- tutorial02_red_triangle/tutorial02.cpp
+++ tutorial02_red_triangle/tutorial02.cpp
@@ -610,13 +610,13 @@ int main( void )
     // Our vertices. Tree consecutive floats give a 3D vertex; Three consecutive vertices give a triangle.
     // A cube has 6 faces with 2 triangles each, so this makes 6*2=12 triangles, and 12*3 vertices
     static const GLfloat g_vertex_buffer_data[] = {
-            -1.0f,1.0f, 0.0f,
-            -1.0f,-1.0f,0.0f,
-            1.0f, -1.0f, 0.0f,
+            -1.28125f,1.28125f, 0.0f,
+            -1.28125f,-1.28125f,0.0f,
+            1.28125f, -1.28125f, 0.0f,
 
-            -1.0f,1.0f, 0.0f,
-            1.0f, -1.0f, 0.0f,
-            1.0f,1.0f,0.0f,
+            -1.28125f,1.28125f, 0.0f,
+            1.28125f, -1.28125f, 0.0f,
+            1.28125f,1.28125f,0.0f,
 
 
 
