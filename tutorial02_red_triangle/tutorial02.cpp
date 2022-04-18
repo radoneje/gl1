4f99de3127ef1c3f5b9cf71faa5ea2fd747424c8
radoneje <den.shevchenko@gmail.com>
2022-04-18 22:09:51 +0300

    ddddf

--- tutorial02_red_triangle/tutorial02.cpp
+++ tutorial02_red_triangle/tutorial02.cpp
@@ -377,8 +377,8 @@ int main(void) {
         //GLuint Texture = loadDDS("uvtemplate.DDS");
 
         finalFrameData_lock.lock();
-        std::cout << "render frame, width: " <<  << " " << finalFrameData.frameNumber << std::endl;
-        glTexSubImage2D(GL_TEXTURE_2D, 0, 0,0, finalFrameData.width, finalFrameData.height, GL_RGB, GL_UNSIGNED_BYTE, finalFrameData.data[0]);
+        std::cout << "render frame: " << finalFrameData.frameNumber << std::endl;
+        glTexSubImage2D(GL_TEXTURE_2D, 0, 0,0, finalFrameData.width, finalFrameData.height, GL_RGB, GL_UNSIGNED_BYTE, finalFrameData.data);
 
         finalFrameData_lock.unlock();
         // Get a handle for our "myTextureSampler" uniform
