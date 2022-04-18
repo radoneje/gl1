60f65a7526c83f4b8029679b2aeaf2bda52b3eb9
radoneje <den.shevchenko@gmail.com>
2022-04-18 14:41:11 +0300

    ddddf

--- tutorial02_red_triangle/tutorial02.cpp
+++ tutorial02_red_triangle/tutorial02.cpp
@@ -625,9 +625,9 @@ int main( void )
 
     // Two UV coordinatesfor each vertex. They were created with Blender.
     static const GLfloat g_uv_buffer_data[] = {
-            0.000059f, 1.0f-0.000004f,
-            0.000103f, 1.0f-0.336048f,
-            0.335973f, 1.0f-0.335903f,
+            0.0f, 1.0f,
+            0.0f, 0.0f,
+            1.0f, 0.0f,
             1.000023f, 1.0f-0.000013f,
             0.667979f, 1.0f-0.335851f,
             0.999958f, 1.0f-0.336064f,
