63e38b3aee5b744900a048c2666bf3e160b64314
radoneje <den.shevchenko@gmail.com>
2022-04-18 13:34:45 +0300

    ddddf

--- tutorial02_red_triangle/tutorial02.cpp
+++ tutorial02_red_triangle/tutorial02.cpp
@@ -591,7 +591,7 @@ int main( void )
     glm::mat4 Projection = glm::perspective(glm::radians(45.0f), 16.0f / 9.0f, 0.1f, 50.0f);
     // Camera matrix
     glm::mat4 View       = glm::lookAt(
-            glm::vec3(1,3,3), // Camera is at (4,3,3), in World Space
+            glm::vec3(1,1,3), // Camera is at (4,3,3), in World Space
             glm::vec3(0,0,0), // and looks at the origin
             glm::vec3(0,1,0)  // Head is up (set to 0,-1,0 to look upside-down)
     );
