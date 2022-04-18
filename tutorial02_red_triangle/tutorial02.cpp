4e5325ab6468858c59b8fe605f91072a9a1e5f3f
radoneje <den.shevchenko@gmail.com>
2022-04-18 17:19:32 +0300

    ddddf

--- tutorial02_red_triangle/tutorial02.cpp
+++ tutorial02_red_triangle/tutorial02.cpp
@@ -178,7 +178,10 @@ int work(){
                         break;
                     }
                     ii++;
-                    std::cout << "start_time_realtime " <<  frame->pkt_dts << std::endl;;// << vid_stream->avg_frame_rate.num << std::endl;
+                    int64_t pts = av_rescale(frame->dts, 1000000, AV_TIME_BASE);
+                    int64_t now = av_gettime_relative() ;//- frame->start;
+
+                    std::cout << "start_time_realtime " << pts << " "<< now << " " << frame->dts << std::endl;;// << vid_stream->avg_frame_rate.num << std::endl;
                    // int64_t pts = av_rescale(ist->dts, 1000000, AV_TIME_BASE);
                     /////////
                   //  std::cout << "frame: " << ctx_codec->frame_number << std::endl;
