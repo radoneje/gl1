d1c345834a7de0b2aa67b9c15c018c9cc2100cf5
radoneje <den.shevchenko@gmail.com>
2022-04-18 14:20:48 +0300

    ddddf

--- tutorial02_red_triangle/tutorial02.cpp
+++ tutorial02_red_triangle/tutorial02.cpp
@@ -593,7 +593,7 @@ int main( void )
     glm::mat4 View       = glm::lookAt(
             glm::vec3(0,0,4), // Camera is at (4,3,3), in World Space
             glm::vec3(0,0,0), // and looks at the origin
-            glm::vec3(0,1.9f,0)  // Head is up (set to 0,-1,0 to look upside-down)
+            glm::vec3(0,0,0)  // Head is up (set to 0,-1,0 to look upside-down)
     );
     // Model matrix : an identity matrix (model will be at the origin)
     glm::mat4 Model      = glm::mat4(1.0f);
