95516f50b846200e73989931a9e7346d7693b391
radoneje <den.shevchenko@gmail.com>
2022-04-19 21:36:27 +0300

    ddddf

--- tutorial02_red_triangle/tutorial02.cpp
+++ tutorial02_red_triangle/tutorial02.cpp
@@ -597,7 +597,7 @@ void Reshape (int width, int height)
 void Display ()
 {
    // std::cout << "Display";
-    glClearColor (255.0, 1.0, 1.0, 0.0);
+    glClearColor (0.0, 0.0, 1.0, 0.0);
     glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glEnable (GL_TEXTURE_2D);
     glTexEnvf (GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
