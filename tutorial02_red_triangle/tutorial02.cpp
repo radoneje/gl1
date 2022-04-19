e93a95269f8db7d90935ab464fd34ac6a56758e5
radoneje <den.shevchenko@gmail.com>
2022-04-19 17:13:15 +0300

    ddddf

--- tutorial02_red_triangle/tutorial02.cpp
+++ tutorial02_red_triangle/tutorial02.cpp
@@ -601,13 +601,13 @@ void Display ()
 
     glBegin(GL_QUADS);
     glTexCoord2f(0,1);
-    glVertex3f(-5.5,5,-6);
+    glVertex3f(-5.5,5,-8);
     glTexCoord2f(0,0);
-    glVertex3f(-5.5,-5,-6);
+    glVertex3f(-5.5,-5,-8);
     glTexCoord2f(1,0);
-    glVertex3f(5,-5,-6);
+    glVertex3f(5,-5,-8);
     glTexCoord2f(1,1);
-    glVertex3f(5,5,-6);
+    glVertex3f(5,5,-8);
     glEnd();
     glDisable(GL_TEXTURE_2D);
 
