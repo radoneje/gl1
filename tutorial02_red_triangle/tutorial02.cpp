c566ec058660e33601a264a66e48561dbcf9ce66
radoneje <den.shevchenko@gmail.com>
2022-04-19 21:31:25 +0300

    ddddf

--- tutorial02_red_triangle/tutorial02.cpp
+++ tutorial02_red_triangle/tutorial02.cpp
@@ -603,13 +603,13 @@ void Display ()
     glBindTexture (GL_TEXTURE_2D, ImageOne.texName);
 
     glBegin(GL_QUADS);
-    glTexCoord2f(0,1);
-    glVertex3f(-5.5,5,-8);
-    glTexCoord2f(1,1);
-    glVertex3f(-5.5,-5,-8);
-    glTexCoord2f(0,1);
-    glVertex3f(5,-5,-8);
     glTexCoord2f(0,0);
+    glVertex3f(-5.5,5,-8);
+    glTexCoord2f(0,1);
+    glVertex3f(-5.5,-5,-8);
+    glTexCoord2f(1,0);
+    glVertex3f(5,-5,-8);
+    glTexCoord2f(1,1);
     glVertex3f(5,5,-8);
     glEnd();
     glDisable(GL_TEXTURE_2D);
